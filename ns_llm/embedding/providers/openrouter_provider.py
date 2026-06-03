6735a350489396a8d92a2a2cb50f5492f46caaf1
James08-dot <james.usana2008@gmail.com>
2026-06-03 09:11:14 +0200

    Provider embedding

---
+++ ns_llm/embedding/providers/openrouter_provider.py
@@ -0,0 +1,10 @@
+from openrouter import OpenRouter
+
+def call_openrouter(    
+    model: str,
+    text: str,
+    input_type: str,
+    dimensions: int,
+    api_key: str
+) -> dict:
+    raise NotImplementedError("Da implementare")
---
+++ ns_llm/embedding/providers/together_provider.py
@@ -0,0 +1,10 @@
+from together import Together
+
+def call_together(    
+    model: str,
+    text: str,
+    input_type: str,
+    dimensions: int,
+    api_key: str
+) -> dict:
+    raise NotImplementedError("Da implementare")
