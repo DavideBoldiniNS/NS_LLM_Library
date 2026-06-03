870e438f7e8c746c1cf33f41d9e9727f37d45680
Gabriele Facun <gabrielefacun@gmail.com>
2026-06-03 16:48:50 +0200

    Inizio implementazione together_provider (da finire)

--- ns_llm/embedding/providers/together_provider.py
+++ ns_llm/embedding/providers/together_provider.py
@@ -7,4 +7,15 @@ def call_together(
     dimensions: int,
     api_key: str
 ) -> dict:
-    raise NotImplementedError("Da implementare")
+    client=Together(api_key=api_key)
+    params={
+        "model": model,
+        "input": text,
+    }
+    if "supporta-input-type" in model:
+        params["input_type"]=input_type
+    response=client.embeddings.create(**params)
+    return{
+        "embedding":response.data[0].embedding,
+        "input_tokens":0,
+    }
